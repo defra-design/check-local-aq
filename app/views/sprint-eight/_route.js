b24baec1ddb92c8b986bdfdc1b2586007e583126
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2024-05-17 15:20:22 +0100

    Publish sprint-eight

--- app/views/sprint-eight/_route.js
+++ app/views/sprint-eight/_route.js
@@ -1,10 +1,10 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit');
-const router = govukPrototypeKit.requests.setupRouter('/sprint-eight'); 
+const router = govukPrototypeKit.requests.setupRouter('/post-mvp'); 
 const locationController = require('./controllers/location.js'); 
 const airQualityModule = require('./data/air-quality.js');
 
-const version = 'sprint-eight';
+const version = 'post-mvp';
 
 // Location search
 router.get('/' + version + '/where', (req, res) => {
