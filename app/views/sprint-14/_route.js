4ff3adf4cb7e0f8c76227ad067a8a25234420c28
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2024-09-03 14:14:05 +0100

    Sprint 14

--- app/views/sprint-14/_route.js
+++ app/views/sprint-14/_route.js
@@ -39,18 +39,77 @@ router.get('/alerts/:slug', (req, res) => {
   alertsController.getAlertBySlug(req, res, version);
 });
 
+// Confirm location post location forecast
+
 router.get('/sign-up-for-alerts/confirm-location', (req, res) => {
   const locationName = req.query.locationName;
   const localAuthority = req.query.localAuthority;
-  const locationString = locationName + ", " + localAuthority;
+  const locationString = `${locationName}, ${localAuthority}`;
+
+  // Store the locationString in the session
+  req.session.data['locationString'] = locationString;
 
   res.render(version + '/sign-up-for-alerts/confirm-location', {
-      locationName: locationName,
-      localAuthority: localAuthority,
-      locationString: locationString 
+    locationName: locationName,
+    localAuthority: localAuthority,
+    locationString: locationString
   });
 });
 
+
+router.post('/sign-up-for-alerts/type-of-notifications', function(req, res) {
+  const selectedOption = req.body.where;
+
+  if (selectedOption === '/' + version + '/sign-up-for-alerts/type-of-notifications') {
+    const locationString = req.session.data['locationString'];
+    req.session.data['locationString'] = locationString;
+    res.redirect(selectedOption);
+  } else if (selectedOption === '/' + version + '/sign-up-for-alerts/where-do-you-want-alerts-for') {
+    res.redirect(selectedOption);
+  } 
+});
+
+router.post('/sign-up-for-alerts/method-of-notification', function(req, res) {
+  let selectedNotifications = req.body['air-quality-alerts'];
+
+  if (!Array.isArray(selectedNotifications)) {
+    selectedNotifications = selectedNotifications ? [selectedNotifications] : [];
+  }
+  selectedNotifications = selectedNotifications.filter(notification => !notification.includes('_unchecked'));
+
+  req.session.data['selectedNotifications'] = selectedNotifications;
+
+  res.redirect('/' + version + '/sign-up-for-alerts/method-of-notification');
+});
+
+
+router.post('/sign-up-for-alerts/check-your-email', function(req, res) {
+  const contactMethod = req.body.contact;
+
+  req.session.data['contactMethod'] = contactMethod;
+
+  if (contactMethod === 'email') {
+    req.session.data['notifyByEmail'] = req.body.notifyByEmail;
+    res.redirect('/' + version + '/sign-up-for-alerts/check-your-email');
+  } else if (contactMethod === 'text') {
+    req.session.data['notifyByText'] = req.body.notifyByText;
+    res.redirect('/' + version + '/sign-up-for-alerts/check-your-messages');
+  } 
+});
+
+
+
+router.get('/sign-up-for-alerts/check-your-email', function(req, res) {
+  const email = req.session.data['notifyByEmail'];
+  const locationString = req.session.data['locationString'];
+  const selectedNotifications = req.session.data['selectedNotifications']; 
+  
+  res.render(version + '/sign-up-for-alerts/check-your-email', {
+    email: email,
+    locationString: locationString,
+    selectedNotifications: selectedNotifications,
+  });
+});
 
 
 
