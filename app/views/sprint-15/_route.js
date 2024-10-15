de6a204751cfd293dd05081ab2f589801c5e2369
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2024-10-15 17:35:53 +0100

    Publish sprint 15

---
+++ app/views/sprint-15/_route.js
@@ -0,0 +1,161 @@
+const govukPrototypeKit = require('govuk-prototype-kit');
+const router = govukPrototypeKit.requests.setupRouter('/sprint-15');
+const locationController = require('./controllers/location.js'); 
+const airQualityModule = require('./data/air-quality.js');
+const alertsController = require('./controllers/alerts.js');
+const { monitoringSites, siteTypeDescriptions } = require('./data/monitoring-sites.js');  
+
+const version = 'sprint-15';
+
+// Location search
+router.get('/' + version + '/where', (req, res) => {
+  res.render(version + '/where', {
+    airQuality: airQualityModule.airQuality,
+  });
+});
+
+// One location returned in query
+router.post('/location', (req, res) => {
+  locationController.getLocationData(req, res, version);
+});
+
+// Disambiguation
+router.get('/location/:id', (req, res) => {
+  locationController.getLocationDetails(req, res, version);
+});
+
+router.get('/health-effects', (req, res) => {
+  res.render('/' + version + '/health-effects', {
+    airQualityData: airQualityModule.commonMessages,
+  });
+});
+
+// Alerts route
+router.get('/alerts', (req, res) => {
+  alertsController.getAlerts(req, res, version);
+});
+
+// Individual alert route
+router.get('/alerts/:slug', (req, res) => {
+  alertsController.getAlertBySlug(req, res, version);
+});
+
+// Confirm location post location forecast
+router.get('/sign-up-for-alerts/confirm-location', (req, res) => {
+  const locationName = req.query.locationName;
+  const parentArea = req.query.parentArea;
+  const locationString = `${locationName}, ${parentArea}`;
+
+  // Store the locationString in the session
+  req.session.data['locationString'] = locationString;
+  req.session.data['locationName'] = locationName;
+  req.session.data['parentArea'] = parentArea;
+
+  res.render(version + '/sign-up-for-alerts/confirm-location', {
+    locationName: locationName,
+    parentArea: parentArea,
+    locationString: locationString
+  });
+});
+
+
+
+
+router.get('/sign-up-for-alerts/check-your-email', function(req, res) {
+  const email = req.session.data['notifyByEmail'];
+  const locationString = req.session.data['locationString'];
+  
+  res.render(version + '/sign-up-for-alerts/check-your-email', {
+    email: email,
+    locationString: locationString,
+  });
+});
+
+router.get('/sign-up-for-alerts/check-your-messages', function(req, res) {
+  const text = req.session.data['notifyByText'];
+  const locationString = req.session.data['locationString'];
+  
+  res.render(version + '/sign-up-for-alerts/check-your-messages', {
+    text: text,
+    locationString: locationString,
+  });
+});
+
+
+// Manage air quality alerts (This is used for a number of screens)
+router.get('/sign-up-for-alerts/manage-alerts/:status', function(req, res) {
+  const email = req.session.data['notifyByEmail'];
+  const text = req.session.data['notifyByText'];
+  const locationString = req.session.data['locationString'];
+  const status = req.params.status;  
+
+  res.render(version + '/sign-up-for-alerts/manage-alerts', {
+    email: email,
+    text: text,
+    locationString: locationString,
+    status: status
+  });
+});
+
+// Email notifications
+router.get('/sign-up-for-alerts/email-notification/:status', function(req, res) {
+  const email = req.session.data['notifyByEmail'];
+  const locationString = req.session.data['locationString'];
+  const status = req.params.status;  
+
+  res.render(version + '/sign-up-for-alerts/email-notification', {
+    email: email,
+    locationString: locationString,
+    status: status
+  });
+});
+
+// Hardcoding the location page so it can be linked to from an email alert
+router.get('/location-forecast', function(req, res) {
+  const locationString = req.session.data['locationString'];
+  const locationName = req.session.data['locationName'] || 'Missing name';
+
+  const airQuality = {
+    today: {
+      value: req.query.todayValue || 3,
+      readableBand: req.query.todayBand || 'low'
+    },
+    day2: {
+      value: req.query.day2Value || 3,
+      readableBand: req.query.day2Band || 'low'
+    },
+    day3: {
+      value: req.query.day3Value || 9,
+      readableBand: req.query.day3Band || 'high'
+    },
+    day4: {
+      value: req.query.day4Value || 9,
+      readableBand: req.query.day4Band || 'high'
+    },
+    day5: {
+      value: req.query.day5Value || 5,
+      readableBand: req.query.day5Band || 'moderate'
+    }
+  };
+
+  const result = {
+    GAZETTEER_ENTRY: {
+      NAME1: locationName
+    }
+  };
+
+
+  res.render(version + '/location-forecast', {
+    airQuality: airQuality,
+    locationName: locationName,
+    locationString: locationString,
+    monitoringSites: monitoringSites,
+    result: result,
+    version: version
+  });
+});
+
+
+module.exports = router;
+
+
---
+++ app/views/sprint-15/data/air-quality.js
@@ -0,0 +1,126 @@
+const airQualityValues = {
+    aqValueToday: "3",
+    aqValueDay2: "3",
+    aqValueDay3: "4",
+    aqValueDay4: "3",
+    aqValueDay5: "3"
+};
+
+const commonMessages = {
+    low: {
+        values: [1, 2, 3],
+        advice: "Enjoy your usual outdoor activities.",
+        atrisk: {
+            adults: "Enjoy your usual outdoor activities.",
+            asthma: "Enjoy your usual outdoor activities.",
+            oldPeople: "Enjoy your usual outdoor activities."
+        },
+        ukToday: "Strong winds for many areas during today will help to keep air pollution levels low across the whole of the UK.",
+        ukTomorrow: "The unsettled weather will continue resulting in low levels of air pollution across the whole of the UK.",
+        ukOutlook: "Strong winds for many areas will help to keep air pollution levels low at the beginning of the period. Later on an influx of warm air from the continent may result in moderate air pollution."
+    },
+    moderate: {
+        values: [4, 5, 6],
+        advice: "For most people, short term exposure to moderate levels of air pollution is not an issue.",
+        atrisk: {
+            adults: "Adults who have heart problems and feel unwell should consider doing less strenuous exercise, especially outside.",
+            asthma: "People with asthma should be prepared to use their reliever inhaler.",
+            oldPeople: "Older people can be more susceptible to the effects of poor air quality and should consider doing less strenuous activity, especially outside."
+        },
+        ukToday: "The influx of warm air from the continent is resulting in moderate air pollution levels throughout many areas today.",
+        ukTomorrow: "Light winds and warm temperatures could result in moderate air pollution levels throughout many areas tomorrow.",
+        ukOutlook: "Strong winds for many areas will help to keep air pollution levels low at the beginning of the period. Later on an influx of warm air from the continent may result in moderate air pollution."
+    },
+    high: {
+        values: [7, 8, 9],
+        advice: "Anyone experiencing discomfort such as sore eyes, cough or sore throat should consider reducing activity, particularly outdoors.",
+        atrisk: {
+            adults: "Adults with heart problems should reduce strenuous physical exertion, particularly outdoors, especially if they experience symptoms.",
+            asthma: "People with asthma may find they need to use their reliever inhaler more often.",
+            oldPeople: "Older people can be more susceptible to the effects of poor air quality and should reduce physical exertion."
+        },
+        ukToday: "Warm temperatures are expected to increase pollution levels to high across many areas today.",
+        ukTomorrow: "Test tomorrrow",
+        ukOutlook: "Test outlook"
+    },
+    veryHigh: {
+        values: [10],
+        advice: "Reduce physical exertion, particularly outdoors, especially if you experience symptoms such as cough or sore throat.",
+        atrisk: {
+            adults: "Adults with heart problems should avoid strenuous physical activity.",
+            asthma: "People with asthma may need to use their reliever inhaler more often.",
+            oldPeople: "Older people can be more susceptible to the effects of poor air quality and should avoid strenuous physical activity."
+        },
+        ukToday: "The current heatwave shows no signs of relenting, causing air pollution levels to remain very high across many areas today.",
+        ukTomorrow: "Test tomorrrow",
+        ukOutlook: "Test outlook"
+        
+    },
+    unknown: {
+        advice: "No data available."
+    }
+};
+
+// Function to get common messages based on the air quality band
+function getCommonMessage(band) {
+    return commonMessages[band] || commonMessages.unknown;
+}
+
+// Function for getting detailed air quality information and advice
+function getDetailedInfo(aqValue) {
+    const lookup = {
+        "1": { band: "low", readableBand: "low" },
+        "2": { band: "low", readableBand: "low" },
+        "3": { band: "low", readableBand: "low" },
+        "4": { band: "moderate", readableBand: "moderate" },
+        "5": { band: "moderate", readableBand: "moderate" },
+        "6": { band: "moderate", readableBand: "moderate" },
+        "7": { band: "high", readableBand: "high" },
+        "8": { band: "high", readableBand: "high" },
+        "9": { band: "high", readableBand: "high" },
+        "10": { band: "veryHigh", readableBand: "very high" }
+    };
+
+    const bandInfo = lookup[aqValue.toString()] || { band: "unknown", readableBand: "unknown" };
+    const message = getCommonMessage(bandInfo.band);
+    return {
+        value: aqValue,
+        band: bandInfo.band,
+        readableBand: bandInfo.readableBand,
+        advice: message.advice,
+        atrisk: message.atrisk,
+        ukToday: message.ukToday, // Consider updating these messages to be relevant for each day
+        ukTomorrow: message.ukTomorrow,
+        ukOutlook: message.ukOutlook
+    };
+}
+
+// Function to get air quality labelling for today and the next 4 days
+function getAirQuality(aqValueToday, aqValueDay2, aqValueDay3, aqValueDay4, aqValueDay5) {
+    return {
+        today: getDetailedInfo(aqValueToday),
+        day2: getDetailedInfo(aqValueDay2),
+        day3: getDetailedInfo(aqValueDay3),
+        day4: getDetailedInfo(aqValueDay4),
+        day5: getDetailedInfo(aqValueDay5)
+    };
+}
+
+// Function for determining the highest air quality value
+function getHighestAQDetails(aqValueToday, aqValueDay2, aqValueDay3, aqValueDay4, aqValueDay5) {
+    const highestAQValue = Math.max(aqValueToday, aqValueDay2, aqValueDay3, aqValueDay4, aqValueDay5);
+    return getAirQuality(highestAQValue, highestAQValue, highestAQValue, highestAQValue, highestAQValue).today; 
+}
+
+module.exports = {
+    getAirQuality,
+    getCommonMessage,
+    commonMessages,
+    airQualityValues,
+    getHighestAQDetails
+};
+
+
+
+
+
